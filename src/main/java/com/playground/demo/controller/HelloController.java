102216cb652f722b61cffdce362197eba0b1eafa
sudheertatapudi <sudheertatapudi9@gmail.com>
2025-10-25 12:52:36 +0530

    first commit basic boot app

---
+++ src/main/java/com/playground/demo/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.playground.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    @GetMapping("/hello/{name}")
+    public String helloWithName(@PathVariable String name) {
+        return "hello " + name;
+    }
+}
